c04de8cc672ba685d27647a49bc46c0e7a552c0a
JonM0 <61517667+JonM0@users.noreply.github.com>
2023-01-12 16:05:48 +0100

    Add circle tests

---
+++ test/sql/test-circle4d.sql
@@ -0,0 +1,10 @@
+select circle4d '((0,0,0,0), 1)';
+select circle4d '((0,1,2,3), 2)';
+select circle4d '((0,0,0,0), -0.8)';
+
+select point4d '(0,0,0,0)' <@ circle4d '((0,0,0,0), 1)';
+select point4d '(0,1,1,0)' <@ circle4d '((0,0,0,0), 1)';
+select point4d '(0,0,1,0)' <@ circle4d '((0,0,0,0), 1)';
+
+select point4d '(0.707,0.707,0,0)' <@ circle4d '((0,0,0,0), 1)';
+select point4d '(0.708,0.708,0,0)' <@ circle4d '((0,0,0,0), 1)';
